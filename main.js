8c2baa40371f4342d9336b0730a63969c419ffb7
Liddy01 <lydianenye@gmail.com>
2023-01-13 19:18:31 -0800

    DOM Manipulation

---
+++ main.js
@@ -0,0 +1,17 @@
+// DOM Manipulation
+
+// GetElementById()
+const title = document.getElementById('main-heading');
+console.log(title)
+
+// GetElementByClassName()
+const ListItem = document.getElementsByClassName('list-items');
+console.log(ListItem)
+
+
+// GetElementByTagName()
+
+
+// querrySelector
+
+// querrySelectorAll

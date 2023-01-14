a66ec6e258837c051a29ce10040023d9eb8db100
Liddy01 <lydianenye@gmail.com>
2023-01-13 19:43:22 -0800

    querry selector

--- main.js
+++ main.js
@@ -5,13 +5,19 @@ const title = document.getElementById('main-heading');
 console.log(title)
 
 // GetElementByClassName()
-const ListItem = document.getElementsByClassName('list-items');
-console.log(ListItem)
+// const ListItem = document.getElementsByClassName('list-items');
+// console.log(ListItem)
 
 
 // GetElementByTagName()
+const ListItem = document.getElementsByTagName('li');
+console.log(ListItem)
 
 
 // querrySelector
+// const container = document.querySelector('div');
+// console.log(container)
 
 // querrySelectorAll
+const container = document.querySelectorAll('div');
+console.log(container)
